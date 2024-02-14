63fbd51fa544bf0bc3d3c7b75664fa46a5871022
Greg Ross <gregandrewross79@gmail.com>
2024-02-14 00:57:08 +0000

    Menu cubes now have the same shadows as the game cubes

--- TileRoll/MenuView/Cubes/CubeNodeViewRepresentable.swift
+++ TileRoll/MenuView/Cubes/CubeNodeViewRepresentable.swift
@@ -15,8 +15,12 @@ struct CubeNodeViewRepresentable: UIViewRepresentable {
         animateCube(cubeNode)
         cubeNode.position.y = -0.5
         let cameraNode = cameraNode()
+        let ambientLightNode = ambientLightNode()
+        let directionalLightNode = directionalLightNode()
         sceneView.scene?.rootNode.addChildNode(cubeNode)
         sceneView.scene?.rootNode.addChildNode(cameraNode)
+        sceneView.scene?.rootNode.addChildNode(ambientLightNode)
+        sceneView.scene?.rootNode.addChildNode(directionalLightNode)
         return sceneView
     }
 
@@ -51,6 +55,29 @@ struct CubeNodeViewRepresentable: UIViewRepresentable {
         cameraNode.position.x = 2.4
         return cameraNode
     }
+    
+    private func ambientLightNode() -> SCNNode {
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .ambient
+        lightNode.light?.color = UIColor.darkGray
+        return lightNode
+    }
+    
+    private func directionalLightNode() -> SCNNode {
+        let initialPosition : SCNVector3 = SCNVector3(x: 0, y: 15, z: 0)
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .directional
+        lightNode.light?.intensity = 1000
+        lightNode.light?.color = UIColor.white.cgColor
+        lightNode.light?.castsShadow = true
+        lightNode.position = initialPosition
+        let rotationAngle = SCNVector3Make(-1, 0, 0)
+        lightNode.eulerAngles = rotationAngle
+        return lightNode
+    }
+    
     
     private func animateCube(_ node : SCNNode) {
         node.runAction(playerCube.animation.action)
