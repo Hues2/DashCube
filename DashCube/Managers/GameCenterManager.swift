d103ac7fa4888ad39b18b7ab673a42faba37c20d
Greg Ross <gregandrewross79@gmail.com>
2024-02-18 12:49:48 +0000

    Fixed functionality in the game center manager to successfully get the high score from the game center leaderboard Game center manager can now set the high score in the game center leaderboard

--- DashCube/Managers/GameCenterManager.swift
+++ DashCube/Managers/GameCenterManager.swift
@@ -3,20 +3,45 @@ import GameKit
 import Combine
 
 class GameCenterManager {
-    @Published private(set) var gcEnabled : Bool = false
+    @Published private(set) var isGameCenterEnabled : Bool = false
     @Published private(set) var highScore : Int = 0
     
     // Leaderboards
     private var classicLeaderboard : GKLeaderboard?
     
+    private var cancellables = Set<AnyCancellable>()
+    
     init() {
+        self.addSubscriptions()
         self.authenticateUser()
+    }
+    
+    func addSubscriptions() {
+        subscribeToGameCenterEnabled()
+    }
+}
+
+// MARK: - Subscribers
+private extension GameCenterManager {
+    func subscribeToGameCenterEnabled() {
+        self.$isGameCenterEnabled
+            .dropFirst()
+            .sink { [weak self] newIsGameCenterEnabled in
+                guard let self else { return }
+                guard newIsGameCenterEnabled else {
+                    self.setSavedHighScore(nil)
+                    return
+                }
+                self.loadClassicLeaderboard()
+            }
+            .store(in: &cancellables)
     }
 }
 
 // MARK: - Load classic leaderboard
 extension GameCenterManager {
     func loadClassicLeaderboard() {
+        print("GAME CENTER --> Loading Classic Leaderboard")
         Task {
             do {
                 let leaderboards = try await GKLeaderboard.loadLeaderboards(IDs: [Constants.GameCenter.classicLeaderboard])
@@ -33,20 +58,23 @@ extension GameCenterManager {
 // MARK: - Get user highscore
 extension GameCenterManager {
     func setSavedHighScore(_ leaderboard : GKLeaderboard?) {
-        self.highScore = max(self.getUserHighScoreFromLeaderboard(leaderboard),
-                             self.getUserHighScoreFromAppStorage())
-        // TODO: If the app storage high score is higher than the leaderboard score, then set this score in the game center leaderboard
-    }
-    
-    func getUserHighScoreFromLeaderboard(_ leaderboard : GKLeaderboard?) -> Int {
-        var score : Int = .zero
-        leaderboard?.loadEntries(for: .global, timeScope: .allTime, range: NSRange(location: 1, length: 10)) { local, entries, count, error in
-            print("LOCAL PLAYER SCORE --> \(local?.formattedScore)")
-            score = local?.score ?? 0
+        print("GAME CENTER --> Getting saved high score")
+        let appStorageHighScore = self.getUserHighScoreFromAppStorage()
+        Task {
+            let leaderboardHighScore = await self.getUserHighScoreFromLeaderboard(leaderboard)
+            self.highScore = max(leaderboardHighScore, appStorageHighScore)
         }
-        return score
+        // TODO: If the app storage high score is higher than the leaderboard score, then set this score in the game center leaderboard --> If game center is enabled
     }
     
+    // MARK: - Leaderboard High Score
+    func getUserHighScoreFromLeaderboard(_ leaderboard : GKLeaderboard?) async -> Int {
+        guard let leaderboard else { return 0 }
+        let entries = try? await leaderboard.loadEntries(for: .global, timeScope: .allTime, range: NSRange(location: 1, length: 10))
+        return entries?.0?.score ?? 0
+    }
+    
+    // MARK: - App Storage High Score
     func getUserHighScoreFromAppStorage() -> Int {
         return UserDefaults.standard.integer(forKey: Constants.UserDefaults.highScore)
     }
@@ -55,9 +83,23 @@ extension GameCenterManager {
 // MARK: - Set high score
 extension GameCenterManager {
     func setNewHighScore(_ score : Int) {
+        print("GAME CENTER --> Setting new high score")
         self.highScore = score
-        // TODO: Save this highscore to game centre account
         UserDefaults.standard.setValue(score, forKey: Constants.UserDefaults.highScore)
+        guard isGameCenterEnabled else { return }
+        // Game center is enabled
+        // Save the new highscore
+        Task {
+            try? await GKLeaderboard.submitScore(score,
+                                                 context: 0,
+                                                 player: GKLocalPlayer.local,
+                                                 leaderboardIDs: [Constants.GameCenter.classicLeaderboard])
+            /*
+             If the high score couldn't be saved to the game center leaderboard,
+             it has been saved to the app storage and can be attempted to be saved
+             to the leaderboard again when a new high score has been set
+             */
+        }
     }
 }
 
@@ -68,12 +110,11 @@ extension GameCenterManager {
             guard error == nil else {
                 // TODO: Handle error case
                 print(error?.localizedDescription ?? "")
+                self.isGameCenterEnabled = false
                 return
             }
             print("GAME CENTER ENABLED")
-            print("LOCAL GAME PLAYER ALIAS --> \(GKLocalPlayer.local.alias)")
-            self.gcEnabled = true
-            self.loadClassicLeaderboard()
+            self.isGameCenterEnabled = true
         }
     }
 }
